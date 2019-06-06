b4dd964b064f4ad56644614b1992923ad826d7c9
C4ioFrn <caiomrtsfranco@outlook.com>
2019-06-06 16:29:41 -0300

    teste

---
+++ src/main/java/com/caio/senai/domain/Clientee.java
@@ -0,0 +1,5 @@
+package com.caio.senai.domain;
+
+public class Clientee {
+
+}
---
+++ src/test/java/com/caio/senai/CursoSenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio.senai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
